42812415e5454e3f5d43cfe1cbcf9648c4e742a3
nanocoaster <nanocoaster@pm.me>
2023-02-21 21:01:55 +0100

    Add logging via log and env_logger crates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use log::info;
+
 mod cpu;
 mod registers;
 mod mmu;
 
 fn main() {
+    env_logger::init();
+    info!("Starting up");
+
     println!("Hello, world!");
+
+    info!("Shutting down");
 }
